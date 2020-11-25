ccdf386a4191e4c65568a3fa8182121d25fc9da0
Mikhail Pushin <mpushin@mont.com>
2020-11-25 20:18:59 +0300

    Base class, config, 3 first steps: - NewKeyboardLayoutStep; - SetTimeZoneStep; - InstallIisStep.

---
+++ Install/Install-Tessa.ps1
@@ -0,0 +1,7 @@
+﻿$OutputEncoding #Для создания консоли надо что-то в нее написать, иначе упадет дальше
+$OutputEncoding = [Text.UTF8Encoding]::UTF8
+$OutputEncoding=[Console]::OutputEncoding
+
+
+Import-Module "$PSScriptRoot\MikhailSP.Tessa.Utils.psm1" -Force -Verbose
+Install-TessaPrerequisites -ServerRoles Web,Sql -TessaVersion v3_5_0 -Verbose
---
+++ Install/MikhailSP.Tessa.Utils.psm1
@@ -0,0 +1,189 @@
+﻿enum Role{
+    Web
+    Chronos
+    Sql
+}
+
+enum Version{
+    v3_2_0
+    v3_5_0
+}
+
+class Step
+{
+    $DebugMode = $False
+
+    [string] $StepName
+    [Role[]] $AvailableInServerRoles
+    [Version[]] $AvailableInTessaVersions
+    [object] $Json
+
+    [void] DoStep([Role[]] $ServerRoles, [Version] $TessaVersion){}
+
+    [void] DoAndLogStep([Role[]] $ServerRoles, [Version] $TessaVersion)
+    {
+        Write-Host -ForegroundColor Yellow $this.StepName
+        if (!$this.RoleIsAvailable($ServerRoles))
+        {
+            Write-Host -ForegroundColor Magenta "Step '$($this.StepName)' is not applicable for server roles. Skipping"
+            continue
+        }
+
+        if ($this.DebugMode)
+        {
+            Write-Host -ForegroundColor Magenta "Debug mode. Skipping step"
+            continue
+        }
+        if ($Null -eq $this.Json)
+        {
+            Write-Host -ForegroundColor Magenta "No config section for step '$( $this.StepName )' in prerequisites.json. Skipping"
+            continue
+        }
+
+        $this.DoStep($ServerRoles, $TessaVersion)
+    }
+    
+    [string] GetValueOrLogError([string] $jsonSection)
+    {
+        [string] $value=$this.Json."$jsonSection";
+        if ($Null -eq $value)
+        {
+            throw "No section '$jsonSection' value in config"
+        }
+        return $value
+    }
+    
+    [Boolean] RoleIsAvailable([Role[]] $ServerRoles)
+    {
+        if ($Null -eq $this.AvailableInServerRoles)
+        {
+            return $True
+        }
+        if ($Null -eq $ServerRoles)
+        {
+            return $False
+        }
+        foreach ($availableRole in $this.AvailableInServerRoles)
+        {
+            foreach ($serverRole in $ServerRoles)
+            {
+                if ($serverRole -eq $availableRole)
+                {
+                    return $True
+                }
+            }
+        }
+        return $False
+    }
+
+    Step([string] $stepName, [object] $json, [Role[]] $availableInServerRoles, [Version[]] $availableInTessaVersions)
+    {
+        $this.StepName = $stepName
+        $this.AvailableInServerRoles = $availableInServerRoles
+        $this.AvailableInTessaVersions = $availableInTessaVersions
+        $this.Json = $json
+    }
+
+    Step([string] $stepName, [object] $json, [Role[]] $availableInServerRoles)
+    {
+        $this.StepName = $stepName
+        $this.AvailableInServerRoles = $availableInServerRoles
+        $this.Json = $json
+    }
+
+    Step([string] $stepName, [object] $json)
+    {
+        $this.StepName = $stepName
+        $this.Json = $json
+    }
+
+    Step([string] $stepName)
+    {
+        $this.StepName = $stepName
+    }
+}
+
+class NewKeyboardLayoutStep : Step
+{
+    NewKeyboardLayoutStep([object] $json): base("Adding new keyboard layout", $json){}
+
+    [void] DoStep([Role[]] $ServerRoles, [Version] $TessaVersion){
+        $newLayout = $this.GetValueOrLogError("layout")
+        $UserLanguageList = New-WinUserLanguageList -Language "en-US";
+        $UserLanguageList.Add($newLayout);
+        Set-WinUserLanguageList -LanguageList $UserLanguageList -Force;
+        Write-Host -ForegroundColor Gray "Added new keyboard layout '$newLayout'";
+    }
+}
+
+class SetTimeZoneStep : Step
+{
+    SetTimeZoneStep([object] $json): base("Setting time zone", $json){}
+
+    [void] DoStep([Role[]] $ServerRoles, [Version] $TessaVersion){
+        $timezone =  $this.GetValueOrLogError("name")
+        Set-TimeZone -Name $timezone;
+        Write-Host -ForegroundColor Gray "Time zone set to '$timezone'";
+    }
+}
+
+class InstallIisStep: Step
+{
+    InstallIisStep([object]$json): base("Installing IIS", $json, @([Role]::Web)){}
+    
+    [void] DoStep([Role[]] $ServerRoles, [Version] $TessaVersion){
+        switch($TessaVersion)
+        {
+            ([Version]::v3_2_0)
+            {
+                Write-Host "Installing IIS with .Net Framework 4.5"
+                Install-WindowsFeature -Name Web-Server, Web-Windows-Auth, Web-Mgmt-Console, Web-Asp-Net45, NET-WCF-HTTP-Activation45 -Restart
+            }    
+            ([Version]::v3_5_0)
+            {
+                Write-Host "Installing IIS without .Net Framework"
+                Install-WindowsFeature -Name Web-Server, Web-Windows-Auth, Web-Mgmt-Console -Restart
+            }    
+            default{throw "Unknown version '$TessaVersion' for step '$($this.StepName)'"}
+        }
+        Write-Host -ForegroundColor Gray "IIS installed";
+    }
+}
+
+function Install-TessaPrerequisites
+{
+    <#
+        Установить пререквизиты (вещи, обязательные для установки Tessa, например, IIS для сервера приложений)
+        .PARAMETER ServerRoles Роли сервера
+        .PARAMETER TessaVersion Версия Тессы, пререквизиты к которой надо поставить
+    #>
+    [CmdletBinding()]
+    param(
+        [Role[]]
+        $ServerRoles,
+        
+        [Version]
+        $TessaVersion
+    )
+
+    Write-Verbose "Installing Tessa $TessaVersion prerequisites for roles $( $ServerRoles|foreach { $_ } )"
+
+    $json = Get-Content "$PSScriptRoot\config\prereq\prerequisites.json" | Out-String | ConvertFrom-Json
+    $commonRole = $json.roles.common
+    $webRole = $json.roles.web
+    $chronosRole = $json.roles.chronos
+    $sqlRole = $json.roles.sql
+
+    [Step[]]$steps = @()
+    $steps += [NewKeyboardLayoutStep]::new($commonRole.'keyboard-layout')
+    $steps += [SetTimeZoneStep]::new($commonRole.'timezone')
+    $steps += [InstallIisStep]::new($webRole.'iis')
+
+
+    foreach ($step in $steps)
+    {
+        $step.DoAndLogStep($ServerRoles, $TessaVersion)
+    }
+}
+
+Export-ModuleMember -Function Install-TessaPrerequisites
