dbb49f404ce0ddd0cba75034ef39f423ba940717
Mikhail Pushin <mpushin@mont.com>
2020-11-27 15:09:50 +0300

    README.md

--- Install/Copy-TessaInstaller.ps1
+++ Install/Copy-TessaInstaller.ps1
@@ -6,4 +6,4 @@ New-Item -ItemType Directory -Force -Path $localPath;
 Get-ChildItem "$remotePath\*" -File | Copy-Item -Destination $localPath;
 Copy-Item -Path "$remotePath\Scripts\Install" -Destination $localPath -Recurse;
 Copy-Item -Path "$remotePath\tessa-$tessaVersion" -Destination $localPath -Recurse;
-Start-Process -FilePath "$localPath\Scripts\Install\install_tessa.bat" -PassThru -Wait;
+Start-Process -FilePath "$localPath\Install\install_tessa.bat" -PassThru -Wait;
